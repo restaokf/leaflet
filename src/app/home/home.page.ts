c3ec548dc65f405f5ae33c0ef919a269a35a452c
restaokf <resta.oktafiati@mail.ugm.ac.id>
2023-09-25 10:22:39 +0700

    Base Project Ionic Leaflet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map!: L.Map;
 
+  constructor() { }
+  //ngOnInit() {
+  //}
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([-7.795306981776558, 110.36868749980938], 12);
+
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(this.map);
+
+    const basemap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 20,
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		});
+
+		const basemap1 = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+			maxZoom: 20,
+			attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		});
+    const basemap2 = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      maxZoom: 20,
+      attribution: '&copy; <a href="https://opentopomap.org/about.html">OpenTopoMap</a> contributors'
+    });
+
+		basemap.addTo(this.map);
+
+    L.marker([-7.795306981776558, 110.36868749980938]).addTo(this.map)
+    .bindPopup('SV')
+    .openPopup();
+
+    /* Control Layer */
+		var baseMaps = {
+      "OpenStreetMap": basemap,
+      "CyclOSM": basemap1,
+      "OpenTopo": basemap2
+    };
+  
+    L.control.layers(baseMaps, ).addTo(this.map);
+
+
+  }
 }
